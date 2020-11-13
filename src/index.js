c19ac8bc56cdf98159916fecfc4b034a040c541d
stacksonstack <staceycarrillo44@gmail.com>
2020-11-12 20:04:15 -0500

    added signup and login forms/ fixed elements display

--- src/index.js
+++ src/index.js
@@ -10,13 +10,24 @@ let addBtn2 = document.getElementById("add-activity2");
 const userName = document.getElementById("username");
 const userList = document.getElementById("user-list");
 const randomBtn = document.getElementById("random-btn");
-
 const showAllActivities = document.getElementById("all-activities");
 const userActivities = document.getElementById("user-activities");
-
 const randomActivityGrid = document.querySelector(".activity-page");
 const userActivityList = document.querySelector(".wrapper");
-const allActivitiesGrid =document.querySelector(".activities-grid")
+const allActivitiesGrid =document.querySelector(".activities-grid");
+const usernameInput = document.getElementById("username-input");
+const emailInput = document.getElementById("email-input");
+const passwordInput = document.getElementById("password-input");
+const form = document.getElementById("form");
+const usernameInputLogin = document.getElementById("login-username-input");
+const emailInputLogin = document.getElementById("login-email-input");
+const passwordInputLogin = document.getElementById("login-password-input");
+const formLogin = document.getElementById("login-form");
+const loginPage = document.getElementById("login-page");
+const signupPage = document.getElementById("signup-page");
+const signupBtn = document.getElementById("signup-button");
+const logout = document.getElementById("logout"); 
+let userId
 
 const allActivities = () => {
   fetch("http://localhost:3000/api/v1/activities")
@@ -26,6 +37,87 @@ const allActivities = () => {
       randomActivity(activityObj);
     });
 };
+function loginUser(){
+    formLogin.addEventListener("submit", (e) => {
+        e.preventDefault()
+        fetch("http://localhost:3000/api/v1/users")
+        .then(resp => resp.json())
+        .then(data => {
+            data.forEach((user) => {
+                if(user.username === usernameInputLogin.value){
+                    userId = user.id;
+                    fetchUserAndActivities(userId);
+                }
+                else{
+                    console.log("didnt work")
+                }
+            })
+        })
+        userActivityList.style.display = "none";
+        randomActivityGrid.style.display = "none";
+        allActivitiesGrid.style.display = "block";
+        loginPage.style.display = "none";
+        signupPage.style.display = "none";
+        logout.innerHTML = "Log Out"
+        userActivities.innerHTML = "My Activities"
+    })   
+}
+
+function logoutButton(){
+  logout.addEventListener("click", (e) => {
+    userActivityList.style.display = "none";
+    randomActivityGrid.style.display = "none";
+    allActivitiesGrid.style.display = "none";
+    loginPage.style.display = "block";
+    signupPage.style.display = "none";
+    logout.innerHTML = " "
+    userId = null;
+    userActivities.innerHTML = " "
+    console.log("logged out")
+    
+})}
+
+function signupButton(){
+  signupBtn.addEventListener("click", (e) => {
+    userActivityList.style.display = "none";
+    randomActivityGrid.style.display = "none";
+    allActivitiesGrid.style.display = "none";
+    loginPage.style.display = "none";
+    signupPage.style.display = "block";
+    logout.innerHTML = " "
+    userActivities.innerHTML = " "
+  })
+
+}
+
+function createNewUser(){
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+      console.log(emailInput.value)
+      console.log(usernameInput.value)
+        fetch("http://localhost:3000/api/v1/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        users: {
+        username: usernameInput.value,
+        email: emailInput.value,
+        password: passwordInput.value
+        }
+    }),
+  })
+  .then(resp => resp.json())
+  .then(data => {
+      console.log(data)
+      userId = data.id
+      fetchUserAndActivities(userId);
+    })
+   
+  })
+  form.reset()
+}
 
 function renderActivity(data) {
   data.forEach((activity) => {
@@ -65,14 +157,14 @@ function addActivity(btn, activity) {
       },
       body: JSON.stringify({
         user_activities: {
-          user_id: 30,
+          user_id: userId,
           activity_id: activity.id,
         },
       }),
     })
       .then((resp) => resp.text())
       .then((data) => {
-        persistsData({ name: activity.name, id: data.id });
+        persistsData({name: activity.name, id: data.id });
       });
   });
 }
@@ -133,30 +225,45 @@ function fetchUserActivity({ activity_id, id }) {
     .then((data) => persistsData({ name: data.name, id: id }));
 }
 
-function AllActivitiesPage(){
+function allActivitiesPage(){
     showAllActivities.addEventListener("click", (e) => {
         randomActivityGrid.style.display = "none";
         userActivityList.style.display = "none";
-        allActivitiesGrid.style.display = "block"
-        console.log("all")
+        allActivitiesGrid.style.display = "block";
+        loginPage.style.display = "none";
+        signupPage.style.display = "none";
+        logout.innerHTML = "Log Out"
+        userActivities.innerHTML = "My Activities"
     })
 }
 
 function landingPage(){
   userActivityList.style.display = "none";
-  randomActivityGrid.style.display = "none"
+  randomActivityGrid.style.display = "none";
+  allActivitiesGrid.style.display = "none";
+  loginPage.style.display = "block";
+  signupPage.style.display = "none";
+  logout.innerHTML = " "
+  userActivities.innerHTML = " "
 }
 
-function UserActivitiesPage(){
+function userActivitiesPage(){
   userActivities.addEventListener('click', (e) => {
-    randomActivityGrid.style.display = "none"
-    allActivitiesGrid.style.display = "none"
-    userActivityList.style.display = "block"
+    randomActivityGrid.style.display = "none";
+    allActivitiesGrid.style.display = "none";
+    userActivityList.style.display = "block";
+    loginPage.style.display = "none";
+    signupPage.style.display = "none";
+    logout.innerHTML = "Log Out"
+    userActivities.innerHTML = "My Activities"
   })
 }
 
 landingPage();
 allActivities();
-fetchUserAndActivities(30);
-AllActivitiesPage();
-UserActivitiesPage();
+allActivitiesPage();
+userActivitiesPage();
+createNewUser();
+loginUser();
+signupButton();
+logoutButton()
